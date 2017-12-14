db9811cf8c3d75c0715c9132696e80617793f367
Kirill Suboch <3dvapper.inbox@gmail.com>
2017-12-14 17:51:54 +0400

    NULL logic

---
+++ ch_02/q_0015.sql
@@ -0,0 +1,25 @@
+USE TSQL2012;
+
+SELECT
+    custid,
+    country,
+    region,
+    city
+FROM Sales.Customers
+WHERE region = N'WA';
+
+SELECT
+    custid,
+    country,
+    region,
+    city
+FROM Sales.Customers
+WHERE region <> N'WA';
+
+SELECT
+    custid,
+    country,
+    region,
+    city
+FROM Sales.Customers
+WHERE region IS NULL;
