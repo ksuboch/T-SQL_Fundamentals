56169e0391822be4dbdbf99db54b7925d56314a4
Kirill Suboch <3dvapper.inbox@gmail.com>
2018-02-09 16:32:46 +0400

    Conected subqueries

---
+++ ch_04/q_0003.sql
@@ -0,0 +1,25 @@
+USE TSQL2012;
+
+SELECT
+    custid,
+    orderid,
+    orderdate,
+    empid
+FROM Sales.Orders AS O1
+WHERE orderid =
+    (SELECT MAX(O2.orderid)
+     FROM Sales.Orders AS O2
+     WHERE O2.custid = O1.custid);
+
+SELECT
+    orderid,
+    custid,
+    val,
+    CAST(100. * val / (SELECT SUM(O2.val)
+                       FROM Sales.OrderValues AS O2
+                       WHERE O2.custid = O1.custid)
+         AS NUMERIC(5, 2)) AS pct
+FROM Sales.OrderValues AS O1
+ORDER BY
+    custid,
+    orderid;
