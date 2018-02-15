1e3b46162d813b1695f07e59167b8580260f536a
Kirill Suboch <3dvapper.inbox@gmail.com>
2018-02-15 15:05:45 +0400

    Options

--- ch_05/q_0011.sql
+++ ch_05/q_0011.sql
@@ -45,3 +45,55 @@ WHERE
 GO
 
 ALTER TABLE Sales.Customers DROP COLUMN address;
+GO
+
+ALTER VIEW Sales.USACusts WITH SCHEMABINDING
+AS
+SELECT
+    custid,
+    companyname,
+    contactname,
+    contacttitle,
+    address,
+    city,
+    region,
+    postalcode,
+    country,
+    phone,
+    fax
+FROM
+    Sales.Customers
+WHERE
+    country = N'США'
+WITH CHECK OPTION;
+GO
+
+INSERT INTO Sales.USACusts(
+    companyname,
+    contactname,
+    contacttitle,
+    address,
+    city,
+    region,
+    postalcode,
+    country,
+    phone,
+    fax
+) VALUES (
+    'Клиент FGHIJ',
+    'Контакт FGHIJ',
+    'Должность FHGIJ',
+    'Адрес FGHIJ',
+    'Лондон',
+    NULL,
+    '12345',
+    'Великобритания',
+    '012-3456789',
+    '012-3456789'
+);
+
+DELETE FROM Sales.Customers
+WHERE custid > 91;
+
+IF OBJECT_ID('Sales.USACusts') IS NOT NULL
+    DROP VIEW Sales.USACusts;
