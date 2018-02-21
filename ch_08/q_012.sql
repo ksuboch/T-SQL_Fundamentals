c7e0d8d80de089b7748ed08a556b8115e845d32c
Kirill Suboch <3dvapper.inbox@gmail.com>
2018-02-21 14:26:50 +0400

    Updating/deleting data using top/offset

---
+++ ch_08/q_012.sql
@@ -0,0 +1,62 @@
+USE TSQL2012;
+
+IF OBJECT_ID('dbo.OrderDetails', 'U') IS NOT NULL
+    DROP TABLE dbo.OrderDetails;
+
+IF OBJECT_ID('dbo.Orders', 'U') IS NOT NULL
+    DROP TABLE dbo.Orders;
+
+CREATE TABLE dbo.Orders
+(
+  orderid        INT          NOT NULL,
+  custid         INT          NULL,
+  empid          INT          NOT NULL,
+  orderdate      DATETIME     NOT NULL,
+  requireddate   DATETIME     NOT NULL,
+  shippeddate    DATETIME     NULL,
+  shipperid      INT          NOT NULL,
+  freight        MONEY        NOT NULL
+    CONSTRAINT DFT_Orders_freight DEFAULT(0),
+  shipname       NVARCHAR(40) NOT NULL,
+  shipaddress    NVARCHAR(60) NOT NULL,
+  shipcity       NVARCHAR(25) NOT NULL,
+  shipregion     NVARCHAR(15) NULL,
+  shippostalcode NVARCHAR(10) NULL,
+  shipcountry    NVARCHAR(15) NOT NULL,
+  CONSTRAINT PK_Orders PRIMARY KEY(orderid)
+);
+GO
+
+INSERT INTO dbo.Orders SELECT * FROM Sales.Orders;
+
+DELETE TOP(50) FROM dbo.Orders;
+
+UPDATE TOP(50) dbo.Orders
+    SET freight += 10.00;
+
+WITH C AS
+(
+    SELECT TOP(50) *
+    FROM dbo.Orders
+    ORDER BY orderid
+)
+DELETE FROM C;
+
+WITH C AS
+(
+    SELECT TOP(50) *
+    FROM dbo.Orders
+    ORDER BY orderid
+)
+UPDATE TOP(50) dbo.Orders
+    SET freight += 10.00;
+
+WITH C AS
+(
+    SELECT *
+    FROM dbo.Orders
+    ORDER BY orderid
+    OFFSET 0 ROWS FETCH FIRST 50 ROWS ONLY
+)
+UPDATE C
+    SET freight += 10.00;
